3498e5d163c3fcc7f2cdb622d17bff8c1933dbb2
HWouters <hendrik.wouters@live.nl>
2014-05-24 16:41:52 +0200

    Add test project

---
+++ Eventually.Test/EventTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eventually.Test
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestMethod]
+        public void WhenRaised_MultipleSubscribersAreNotified()
+        {
+            var changed = new Event<string>();
+
+            string received1 = null;
+            string received2 = null;
+
+            changed.Subscribe(s => received1 = s);
+            changed.Subscribe(s => received2 = s);
+            changed.Raise(null, "hello");
+
+            Assert.AreEqual("hello", received1);
+            Assert.AreEqual("hello", received2);
+        }
+    }
+}
